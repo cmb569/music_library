a638c0c4037824be6dc49ed42d1dd0420a3ffca4
Your Name <you@example.com>
2022-12-21 21:39:47 -0800

    changes to codes in components

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,19 +1,27 @@
-import { useState } from 'react'
+// import { useState } from "react";
 
 function SearchBar(props) {
-    let [searchTerm, setSearchTerm] = useState('')
-
-    return (
-        <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
-
-            <input type="text" placeholder="Enter a search term here" onChange={
-                (e) => setSearchTerm(e.target.value)
-            } />
-
-            <input type="submit" />
-
+    //   let [searchTerm, setSearchTerm] = useState("");
+    
+      return (
+        <form>
+          <input
+            type="text"
+            placeholder="Search Here"
+            onChange={(e) => props.handleSearch(e, e.target.value)}
+          />
+          <input type="submit" />
         </form>
-    )
-}
-
-export default SearchBar
+      );
+    }
+    
+    export default SearchBar;
+    
+    //     <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
+    //       <input
+    //         type="text"
+    //         placeholder="Search Here"
+    //         onChange={(e) => setSearchTerm(e.target.value)}
+    //       />
+    //       <input type="submit" />
+    //     </form>
